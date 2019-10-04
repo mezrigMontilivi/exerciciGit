97da0f4f996a8212992bfd2543892a2c69ba7c47
David Mezcua Rigau <41548791g@institutmontilivi.cat>
2019-10-04 21:16:55 +0200

    Creació del joc i funcionalitats de Buscaminas, Comecocos i Escacs

---
+++ dmezcua_jocs/jocs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecciona un joc:\n1)Buscaminas\n2)Comecocos\n3)Escacs\n4)El Penjat\n5)UNO");
+
+        }
+
+        static void Buscaminas()
+        {
+            Console.WriteLine("Aquí aniría la funcionaltat del Buscaminas.");
+        }
+
+        static void Comecocos()
+        {
+            Console.WriteLine("Aquí aniría la funcionaltat del Comecocos.");
+        }
+
+        static void Escacs()
+        {
+            Console.WriteLine("Aquí aniría la funcionaltat dels Escacs.");
+        }
+
+
+    }
+}
